294cf09c24d68306d59b5f665abfd8930bf9b524
Sterling Hayden <haydensj@rose-hulman.edu>
2024-01-25 21:40:04 -0500

    Removed other formats

--- config/formats.ts
+++ config/formats.ts
@@ -18,7 +18,7 @@ The column value will be ignored for repeat sections.
 */
 
 export const Formats: FormatList = [
-
+/*
 	// S/V Singles
 	///////////////////////////////////////////////////////////////////
 
@@ -5073,4 +5073,5 @@ export const Formats: FormatList = [
 		battle: {trunc: Math.trunc},
 		ruleset: ['HP Percentage Mod', 'Cancel Mod', 'Desync Clause Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100'],
 	},
+*/
 ];
